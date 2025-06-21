7274f97fbabdab48880ec03482d39468ce9a4e73
Zeynep Sanli <zeynep.sanli@inavitas.com>
2025-06-21 20:25:31 +0300

    feat: add ApiProperty for swagger

--- src/areas/dto/create-area.dto.ts
+++ src/areas/dto/create-area.dto.ts
@@ -1,10 +1,13 @@
 import { IsString, IsNotEmpty, IsArray } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAreaDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsArray()
   @IsNotEmpty()
   polygon: number[][][]; // GeoJSON coordinates [[[[lon, lat], [lon, lat], [lon, lat], [lon, lat], [lon, lat]]]]
--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,12 +1,16 @@
 import { IsNotEmpty, IsNumber } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateLocationDto {
+  @ApiProperty()
   @IsNotEmpty()
   user_id: string;
 
+  @ApiProperty()
   @IsNumber()
   latitude: number;
 
+  @ApiProperty()
   @IsNumber()
   longitude: number;
 }
